76e435ebed04faebfecdf2d74f0721a2be6571d9
JakeGoh <40029130+JakeGoh@users.noreply.github.com>
2020-03-26 12:45:10 +0800

    Update AP_Proximity_TeraRangerTower.cpp

--- libraries/AP_Proximity/AP_Proximity_TeraRangerTower.cpp
+++ libraries/AP_Proximity/AP_Proximity_TeraRangerTower.cpp
@@ -3,55 +3,26 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <AP_HAL/AP_HAL.h>
 #include "AP_Proximity_TeraRangerTower.h"
-#include <AP_SerialManager/AP_SerialManager.h>
 #include <AP_Math/crc.h>
 #include <ctype.h>
 #include <stdio.h>
 
 extern const AP_HAL::HAL& hal;
 
-/*
-   The constructor also initialises the proximity sensor. Note that this
-   constructor is not called until detect() returns true, so we
-   already know that we should setup the proximity sensor
-*/
-AP_Proximity_TeraRangerTower::AP_Proximity_TeraRangerTower(
-    AP_Proximity &_frontend,
-    AP_Proximity::Proximity_State &_state) :
-    AP_Proximity_Backend(_frontend, _state)
-{
-    const AP_SerialManager &serial_manager = AP::serialmanager();
-
-    uart = serial_manager.find_serial(AP_SerialManager::SerialProtocol_Lidar360, 0);
-    if (uart != nullptr) {
-        uart->begin(serial_manager.find_baudrate(AP_SerialManager::SerialProtocol_Lidar360, 0));
-    }
-}
-
-// detect if a TeraRanger Tower proximity sensor is connected by looking for a configured serial port
-bool AP_Proximity_TeraRangerTower::detect()
-{
-    AP_HAL::UARTDriver *uart = nullptr;
-    uart = AP::serialmanager().find_serial(AP_SerialManager::SerialProtocol_Lidar360, 0);
-    return uart != nullptr;
-}
-
 // update the state of the sensor
 void AP_Proximity_TeraRangerTower::update(void)
 {
-    if (uart == nullptr) {
+    if (_uart == nullptr) {
         return;
     }
 
